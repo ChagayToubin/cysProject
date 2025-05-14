c3eed7b6fffe19105bc4ec6941e34cd20066572d
chagay <chagaytoubin@gmail.com>
2025-05-14 15:56:43 +0300

    update chagay

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("chagay");
+
+            
         }
+        static void OverallView(Dictionary<string, int> MessmessagePoint) 
+        {
+            Console.WriteLine("chagay");
+        }
+
     }
 }
