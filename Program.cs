87c7f9da03e4e67fa7208c7e7407faf7281058d6
Yitzchak_Damen <yitzchakdamen@gmail.com>
2025-05-14 17:01:43 +0300

    Finished working on the MessageDecoder function - running properly - add a helper function - DecodingLetter

--- Program.cs
+++ Program.cs
@@ -1,31 +1,47 @@
-﻿namespace Atbash
+﻿using System.Reflection.Emit;
+
+namespace Atbash
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string m = "Lfi ulixvh ziv kivkzirmt uli z nzqli zggzxp lm gsv Arlmrhg vmvnb.Gsv ilxpvg fmrgh ziv ivzwb zmw dzrgrmt uli gsv hrtmzo. Ylnyh szev yvvm kozxvw mvzi pvb olxzgrlmh. Mfpsyz urtsgvih ziv hgzmwrmt yb uli tilfmw rmurogizgrlm. Gsv zggzxp droo yv hfwwvm zmw hgilmt -- gsvb dlm’g hvv rg xlnrmt. Dv nfhg hgzb srwwvm zmw pvvk gsv kozm hvxivg fmgro gsv ozhg nlnvmg. Erxglib rh mvzi. Hgzb ivzwb.";
          }
         static void OverallView(Dictionary<string, int> MessmessagePoint) 
         {
             Console.WriteLine("chagay");
         }
       
-        string  MessageDecoder(string EncryptedMessage)
+        static string  MessageDecoder(string EncryptedMessage)
         {
-            return "Message - Experience";
+            string MessageDecoder = "";
+            foreach (char letter in EncryptedMessage.ToLower())
+            {
+                if (char.IsLetter(letter))
+                {
+                    MessageDecoder += DecodingLetter(letter);
+                }
+                else
+                {
+                    MessageDecoder += letter;
+                }
+            }
+            return MessageDecoder;
+        }
+
+        static char DecodingLetter(char Letter)
+        {
+            int formula = 'z' - (Letter - 'a');
+            char DecodingLetter = Convert.ToChar(formula);
+            return DecodingLetter;
         }
 
         static Dictionary<string, int> DangerousWordsCalculator(string messege)
         {
             var messagePoints = new Dictionary<string, int> { { "a", 1 } };
             return messagePoints;
-
-
         }
-
-
-
-
     }
 }
