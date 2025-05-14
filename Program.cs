ec10d25caf29ee0f714d5a484cc09d8346188f98
Yitzchak_Damen <yitzchakdamen@gmail.com>
2025-05-14 15:57:28 +0300

    Adding a function MessageDecoder!:)

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("YitzchakDamen");
+        }
+        string  MessageDecoder(string EncryptedMessage)
+        {
+            return "Message - Experience";
         }
     }
 }
