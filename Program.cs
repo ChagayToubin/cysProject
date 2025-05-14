8463aa4b8886bf162fc94618585d7f2b2466a488
tzirkus <zalmen5779@gmail.com>
2025-05-14 17:32:37 +0300

    update dengeros carculate

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-         }
+            PrintDictionary(DangerousWordsCalculator("hey zalmen ma", ["hey", "ma"]));
+        }
         static void OverallView(Dictionary<string, int> MessmessagePoint) 
         {
             Console.WriteLine("chagay");
@@ -16,12 +16,43 @@
             return "Message - Experience";
         }
 
-        static Dictionary<string, int> DangerousWordsCalculator(string messege)
+        
+        
+        static Dictionary<string, int> DangerousWordsCalculator(string messege, string[] dangerousWords)
         {
-            var messagePoints = new Dictionary<string, int> { { "a", 1 } };
+            string temp = "";
+            int sumWords = 0;
+            for (int i = 0; i < messege.Length; i++)
+            {
+                if (messege[i] == ' ')
+                {
+                    sumWords += dangerousWords.Contains(temp) ? 1 : 0;
+                    temp = "";
+                }
+                else if (i == messege.Length - 1)
+                {
+                    temp += messege[i];
+                    sumWords += dangerousWords.Contains(temp) ? 1 : 0;
+                }
+                else
+                {
+                    temp += messege[i];
+                }
+            }
+            var messagePoints = new Dictionary<string, int> { { messege, sumWords } };
+
+
             return messagePoints;
 
 
+        }
+
+        static void PrintDictionary(Dictionary<string, int> dict)
+        {
+            foreach (KeyValuePair<string, int> item in dict)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
         }
 
 
